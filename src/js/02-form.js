6a34dc9b1dba41a54d1789cb8600047fae139a8e
nurgul4261 <nurguloksuzoglu@gmail.com>
2025-10-03 12:03:45 +0300

    Update 02-form.js

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,8 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('.feedback-form').addEventListener('submit', onFormSubmit);
-});
-
-
 function onFormSubmit(event) {
   event.preventDefault();
 
@@ -27,27 +22,5 @@ function onFormSubmit(event) {
 }
 
 
-
-function onFormSubmit(event) {
-  event.preventDefault();
-
-  const form = event.currentTarget;
-  const email = form.elements.email.value.trim();
-  const message = form.elements.message.value.trim();
-
-  if (!email || !message) {
-    alert('Lütfen tüm alanları doldurun!');
-    return;
-  }
-
-  const formData = {
-    email,
-    message,
-    };
- 
-  console.log(formData);
-
-  localStorage.removeItem('feedback-form-state');
-  form.reset();
 }
 
